2d100b05b54c3940eb6c47b946cc1f36c1e277db
Mahi Kandari <mahikandari.ntl@gmail.com>
2026-08-05 12:45:51 +0530

    Time: 572 ms (22.61%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,4 +1,1 @@
-# Write your MySQL query statement below
-select name 
-from customer 
-where (referee_id is null or referee_id != 2);
+select name  from customer where (referee_id is null or referee_id != 2);
