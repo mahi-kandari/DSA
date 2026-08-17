9caa590ee8fbae009b07f7bae76e0f020dbb0ea9
Mahi Kandari <mahikandari.ntl@gmail.com>
2026-08-18 00:42:36 +0530

    Time: 7 ms (60.52%), Space: 13.1 MB (61.33%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    int solve(string &word1, string &word2, int ind1, int ind2, vector<vector<int>> &rec) {
-        if (ind1 == word1.size()) return word2.size() - ind2;
-        if (ind2 == word2.size()) return word1.size() - ind1;
+    int minDistance(string word1, string word2) {
+        int m = word1.size();
+        int n = word2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
         
-        if (rec[ind1][ind2] != -1) return rec[ind1][ind2];
-        
-        int same = 1e8, insert = 1e8, del = 1e8, rep = 1e8;
-        
-        if (word1[ind1] == word2[ind2]) {
-            same = solve(word1, word2, ind1 + 1, ind2 + 1, rec);
-        } else {
-            insert = 1 + solve(word1, word2, ind1, ind2 + 1, rec);
-            del = 1 + solve(word1, word2, ind1 + 1, ind2, rec);
-            rep = 1 + solve(word1, word2, ind1 + 1, ind2 + 1, rec);
+        for(int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for(int j = 0; j <= n; j++) {
+            dp[0][j] = j;
         }
         
-        return rec[ind1][ind2] = min({insert, del, rep, same});
-    }
-
-    int minDistance(string word1, string word2) {
-        vector<vector<int>> rec(word1.size(), vector<int>(word2.size(), -1));
-        return solve(word1, word2, 0, 0, rec);
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(word1[i - 1] == word2[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1]; 
+                } else {
+                    dp[i][j] = min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]}) + 1;
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 };
