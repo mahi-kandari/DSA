91749ec5da6e4acc9cd3587c04e7fc290d85f50a
Mahi Kandari <mahikandari.ntl@gmail.com>
2026-09-02 03:17:57 +0530

    Time: 0 ms (100%), Space: 9.6 MB (88.52%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,23 +1,15 @@
 class Solution {
 public:
-    int ncr(int n , int r){
-        int res = 1;
-        for(int i=0;i<r;i++){
-            res = res* (n-i);
-            res = res/(i+1);
-        }
-        return res;
-    }
     vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> arr;
-        for(int i=1;i<=numRows;i++){
-            vector<int> a;
-            for(int j = 1;j<=i;j++){
-                int k = ncr(i-1,j-1);
-                a.push_back(k);
+        vector<vector<int>> ans;
+        for(int i =0;i<numRows;i++){
+            vector<int> a(i+1,1);
+            for(int j =1;j<i;j++){
+                a[j] = ans[i-1][j-1] + ans[i-1][j];
+                
             }
-            arr.push_back(a);
+            ans.push_back(a);
         }
-        return arr;
+        return ans;
     }
 };
