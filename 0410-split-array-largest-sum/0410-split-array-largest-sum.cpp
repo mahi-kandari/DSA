3666caa075568e380df0106eab51485557e11986
Mahi Kandari <mahikandari.ntl@gmail.com>
2026-09-23 15:13:52 +0530

    Time: 235 ms (4.57%), Space: 12.2 MB (5.6%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,31 +1,28 @@
 class Solution {
 public:
-    int splitArray(vector<int>& nums, int k) {
-        int n = nums.size();
-        int low = *max_element(nums.begin(),nums.end());
-        int high = accumulate(nums.begin(),nums.end(),0);
-        int ans = high;
-        while (low<=high){
-            int mid = low + (high-low)/2;
-            int sub = 1;
-            int count = 0;
-            for(int i =0;i<n;i++){
-                count += nums[i];
-                if (count > mid){
-                    count = nums[i];
-                    sub++;
+    int splitArray(vector<int>& arr, int k) {
+        int n = arr.size();
+        
+        vector<vector<long long>> dp(k + 1, vector<long long>(n + 1, INT_MAX));
+
+        long long sum = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            sum += arr[i];
+            dp[1][i] = sum;
+        }
+
+        for (int s = 2; s <= k; s++) {
+            for (int i = 0; i <= n - s; i++) {
+                long long currsum = 0;
+                for (int j = i; j <= n - s; j++) {
+                    currsum += arr[j];
+                    long long remainingsum = dp[s - 1][j + 1];
+                    long long a = max(currsum, remainingsum);
+                    dp[s][i] = min(dp[s][i], a);
                 }
-
-            }
-            if (sub > k){
-                low = mid+1;
-            }
-            else{
-                ans = mid;
-                high  = mid-1;
-
             }
         }
-        return ans;
+
+        return dp[k][0];
     }
 };
