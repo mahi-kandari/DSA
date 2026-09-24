f67fbd9a96e8b63c73b2ee24aee9907794a75b60
Mahi Kandari <mahikandari.ntl@gmail.com>
2026-09-24 13:43:54 +0530

    Time: 0 ms (100%), Space: 10.4 MB (73.49%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
-    int splitArray(vector<int>& arr, int k) {
-        int n = arr.size();
-        
-        vector<vector<long long>> dp(k + 1, vector<long long>(n + 1, INT_MAX));
-
-        long long sum = 0;
-        for (int i = n - 1; i >= 0; i--) {
-            sum += arr[i];
-            dp[1][i] = sum;
-        }
-
-        for (int s = 2; s <= k; s++) {
-            for (int i = 0; i <= n - s; i++) {
-                long long currsum = 0;
-                for (int j = i; j <= n - s; j++) {
-                    currsum += arr[j];
-                    long long remainingsum = dp[s - 1][j + 1];
-                    long long a = max(currsum, remainingsum);
-                    dp[s][i] = min(dp[s][i], a);
-                }
+    bool solve(int mid, vector<int> &nums, int k){
+        int n = nums.size();
+        int count =1;
+        int book = nums[0];
+        for(int i =1;i<n;i++){
+            if(nums[i]+book <= mid){
+                book += nums[i];
+            }
+            else{
+                count++;
+                book = nums[i];
             }
         }
-
-        return dp[k][0];
+        return count <= k;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(),nums.end());
+        int high  = accumulate(nums.begin(),nums.end(),0);
+        int ans = high;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(solve(mid,nums,k)){
+                ans = mid;
+                high = mid-1;
+            }
+            else low = mid+1;
+        }
+        return ans;
     }
 };
